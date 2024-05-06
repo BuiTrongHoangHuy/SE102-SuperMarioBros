f6b0cc03c338bd056b16aa74f7ea34e86356b58c
BuiTrongHoangHuy <22520533@gm.uit.edu.vn>
2024-05-07 06:34:11 +0700

    add Pipe, new floor draw half map

--- SE102-SuperMarioBros/PlayScene.cpp
+++ SE102-SuperMarioBros/PlayScene.cpp
@@ -12,6 +12,7 @@
 #include "BackGroundObject.h"
 #include "SampleKeyEventHandler.h"
 #include "Gift.h"
+#include "Pipe.h"
 using namespace std;
 
 CPlayScene::CPlayScene(int id, LPCWSTR filePath):
@@ -120,7 +121,7 @@ void CPlayScene::_ParseSection_OBJECTS(string line)
 	case OBJECT_TYPE_BRICK: obj = new CBrick(x,y); break;
 	case OBJECT_TYPE_COIN: obj = new CCoin(x, y); break;
 	case OBJECT_TYPE_GIFT: obj = new CGift(x, y); break;
-
+	case OBJECT_TYPE_PIPE:	obj = new CPipe(x, y); break;
 	case OBJECT_TYPE_PLATFORM:
 	{
 
