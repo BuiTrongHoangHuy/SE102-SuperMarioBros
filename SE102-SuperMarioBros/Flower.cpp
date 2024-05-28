6ac35110d870ebb9091d0416e3fb679d72e6e0e4
BuiTrongHoangHuy <22520533@gm.uit.edu.vn>
2024-05-28 16:11:40 +0700

    implement CFlower

--- SE102-SuperMarioBros/Flower.cpp
+++ SE102-SuperMarioBros/Flower.cpp
@@ -1,24 +1,10 @@
-#pragma once
+#include "Flower.h"
+#include "Animations.h"
 
-#include "GameObject.h"
-
-#define FLOWER_STATE_HIDDEN 0
-#define FLOWER_STATE_APPEARING 1
-#define FLOWER_STATE_VISIBLE 2
-#define FLOWER_STATE_DISAPPEARING 3
-
-#define FLOWER_APPEAR_SPEED 0.05f
-#define FLOWER_BBOX_WIDTH 16
-#define FLOWER_BBOX_HEIGHT 32
-
-class CFlower : public CGameObject
+CFlower::CFlower(float x, float y) : CGameObject(x, y)
 {
-    float startY;
-    float endY;
-public:
-    CFlower(float x, float y);
-    virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
-    virtual void Render();
-    virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
-    virtual void SetState(int state);
-};
+    this->startY = y;
+    this->endY = y - FLOWER_BBOX_HEIGHT;
+    this->state = FLOWER_STATE_HIDDEN;
+}
+
--- SE102-SuperMarioBros/Flower.h
+++ SE102-SuperMarioBros/Flower.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#include "GameObject.h"
+
+#define FLOWER_STATE_HIDDEN 0
+#define FLOWER_STATE_APPEARING 1
+#define FLOWER_STATE_VISIBLE 2
+#define FLOWER_STATE_DISAPPEARING 3
+
+#define FLOWER_APPEAR_SPEED 0.05f
+#define FLOWER_BBOX_WIDTH 16
+#define FLOWER_BBOX_HEIGHT 32
+
+class CFlower : public CGameObject
+{
+    float startY;
+    float endY;
+public:
+    CFlower(float x, float y);
+    virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
+    virtual void Render();
+    virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
+    virtual void SetState(int state);
+};
