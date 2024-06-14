07d05d79edec1268433ba112b402574bbb777230
BuiTrongHoangHuy <22520533@gm.uit.edu.vn>
2024-06-14 16:37:37 +0700

    add animation paragoomba jump

--- SE102-SuperMarioBros/Paragoomba.cpp
+++ SE102-SuperMarioBros/Paragoomba.cpp
@@ -1,10 +1,12 @@
 #include "Paragoomba.h"
+#include "BackGroundObject.h"
 
 CParagoomba::CParagoomba(float x, float y) :CGameObject(x, y)
 {
 	this->ax = 0;
 	this->ay = PARAGOOMBA_GRAVITY;
 	die_start = -1;
+	isOnPlatform = false;
 	SetState(PARAGOOMBA_STATE_WALKING);
 }
 
@@ -17,12 +19,18 @@ void CParagoomba::GetBoundingBox(float& left, float& top, float& right, float& b
 		right = left + PARAGOOMBA_BBOX_WIDTH;
 		bottom = top + PARAGOOMBA_BBOX_HEIGHT_DIE;
 	}
+	else if (state == PARAGOOMBA_STATE_READY_JUMP) {
+		left = x - PARAGOOMBA_READY_JUMP_BBOX_WIDTH / 2;
+		top = y - PARAGOOMBA_READY_JUMP_BBOX_HEIGHT / 2 ;
+		right = left + PARAGOOMBA_READY_JUMP_BBOX_WIDTH;
+		bottom = top + PARAGOOMBA_READY_JUMP_BBOX_HEIGHT ;
+	}
 	else
 	{
 		left = x - PARAGOOMBA_BBOX_WIDTH / 2;
 		top = y - PARAGOOMBA_BBOX_HEIGHT / 2;
-		right = left + PARAGOOMBA_BBOX_WIDTH - 1;
-		bottom = top + PARAGOOMBA_BBOX_HEIGHT - 1;
+		right = left + PARAGOOMBA_BBOX_WIDTH ;
+		bottom = top + PARAGOOMBA_BBOX_HEIGHT ;
 	}
 }
 
@@ -36,14 +44,18 @@ void CParagoomba::OnCollisionWith(LPCOLLISIONEVENT e)
 {
 	if (!e->obj->IsBlocking()) return;
 	if (dynamic_cast<CParagoomba*>(e->obj)) return;
-
+	if (dynamic_cast<CBackGroundObject*>(e->obj)) return;
 	if (e->ny != 0)
 	{
 		vy = 0;
+		if (e->ny < 0)	isOnPlatform = true;
 	}
 	else if (e->nx != 0)
 	{
 		vx = -vx;
+	}
+	if (state == PARAGOOMBA_STATE_JUMPING && isOnPlatform) {
+		SetState(PARAGOOMBA_STATE_WALKING);
 	}
 }
 
@@ -52,12 +64,22 @@ void CParagoomba::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 	vy += ay * dt;
 	vx += ax * dt;
 
+	if (state == PARAGOOMBA_STATE_WALKING || state ==PARAGOOMBA_STATE_READY_JUMP) {
+		stepCount++;
+		if (stepCount >= PARAGOOMBA_JUMP_INTERVAL) {
+			SetState(PARAGOOMBA_STATE_READY_JUMP);
+		}
+		if (stepCount >= 1.5 * PARAGOOMBA_JUMP_INTERVAL) {
+			SetState(PARAGOOMBA_STATE_JUMPING);
+			stepCount = 0;
+		}
+	}
 	if ((state == PARAGOOMBA_STATE_DIE) && (GetTickCount64() - die_start > PARAGOOMBA_DIE_TIMEOUT))
 	{
 		isDeleted = true;
 		return;
 	}
-
+	isOnPlatform = false;
 	CGameObject::Update(dt, coObjects);
 	CCollision::GetInstance()->Process(this, dt, coObjects);
 }
@@ -69,6 +91,12 @@ void CParagoomba::Render()
 	if (state == PARAGOOMBA_STATE_DIE)
 	{
 		aniId = ID_ANI_PARAGOOMBA_DIE;
+	}
+	else if (state == PARAGOOMBA_STATE_READY_JUMP) {
+		aniId = ID_ANI_PARAGOOMBA_READY_JUMP;
+	}
+	else if (state == PARAGOOMBA_STATE_JUMPING) {
+		aniId = ID_ANI_PARAGOOMBA_JUMPING;
 	}
 
 	CAnimations::GetInstance()->Get(aniId)->Render(x, y);
@@ -89,6 +117,14 @@ void CParagoomba::SetState(int state)
 		break;
 	case PARAGOOMBA_STATE_WALKING:
 		vx = -PARAGOOMBA_WALKING_SPEED ;
+		break;
+	case PARAGOOMBA_STATE_READY_JUMP:
+		y -= (PARAGOOMBA_READY_JUMP_BBOX_HEIGHT - PARAGOOMBA_BBOX_HEIGHT) / 2;
+		break;
+	case PARAGOOMBA_STATE_JUMPING:	
+		vy = -PARAGOOMBA_JUMP_SPEED_Y;
+		vx = -0.05f;
+		ay = 0.0015f;
 		break;
 	}
 }
